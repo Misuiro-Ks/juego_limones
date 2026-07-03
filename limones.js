43d2a6f74bd50ac33ce771f4839ef25a78f02aae
unknown <fabian.ni.fr@gmail.com>
2026-07-03 00:45:52 -0500

    agregar alerta GAME OVER

--- limones.js
+++ limones.js
@@ -11,12 +11,18 @@ let personajeX=canvas.width/2;
 let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
 let limonY=5;
+let puntaje=0;
+let vidas=3;
+let velocidadCaida=200;
+
+
 
 function iniciar(){
+    setInterval(bajarLimon,velocidadCaida);//primerParametro: Funcion segundoParametro: tiempo en milisegundos
     DibujarSuelo();
     DibujarPersonaje();    
-    DibujarLimon();
     aparecerLimon();
+    
 }
 
 function DibujarSuelo(){
@@ -61,25 +67,40 @@ function DibujarLimon(){
 function bajarLimon(){
     limonY=limonY+10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
+    detectarPiso();
     
 }
 
-function detectarColision(){
+function detectarAtrapado(){
     if(limonX+ANCHO_LIMON>personajeX && 
          limonX<personajeX+ANCHO_PERSONAJE && 
          limonY+ALTURA_LIMON>personajeY && 
          limonY<personajeY+ALTURA_PERSONAJE){
         //alert("ATRAPADO!! ");
         aparecerLimon();
+        puntaje=puntaje+1;
+        let componente=document.getElementById("txtPuntaje");
+        componente.textContent=puntaje;
     
     }
     
 }
 
-function probarAleatorio(){
-    let aleatorio=generarAleatorio(10,80);
-    console.log(aleatorio);
+function detectarPiso(){
+if(limonY+ALTURA_LIMON>canvas.height-ALTURA_SUELO){
+    aparecerLimon();
+    vidas=vidas-1;
+    let componente=document.getElementById("txtVidas");
+    componente.textContent=vidas;
+    
+if (vidas == 0){
+    
+    setTimeout(function(){
+        alert("GAME OVER");
+    }, 50);
+}
+}
 
 }
 
@@ -88,3 +109,4 @@ function aparecerLimon(){
     limonY=0;
     actualizarPantalla();
 }
+
